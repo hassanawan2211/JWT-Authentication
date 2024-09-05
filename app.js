3aadcfe52f80b17ac4a9d8d6dd0e4bc1161f3821
hassanawan2211 <ali.hassan@tanbits.org>
2024-09-05 13:13:25 +0500

    post api for new user register and save in database

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
 const express = require("express");
-
 const app = express();
-
 const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+const crypto = require("crypto");
+const User = require("./models/users");
 
-const User = require('./models/users')
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16); 
 
-mongoose.connect(
-  "mongodb+srv://Tanbits:5Ly980P6V7N6Q4pQ@cluster0.kz3mj.mongodb.net/tutorial"
-)
-.then(()=>{
-    console.log('mongodb connect')
-})
+mongoose
+  .connect(
+    "mongodb+srv://Tanbits:5Ly980P6V7N6Q4pQ@cluster0.kz3mj.mongodb.net/tutorial"
+  )
+  .then(() => {
+    console.log("mongodb connected");
+  });
 
-app.get("/", function (req, res) {
-  res.send("helo");
+// post user api
+app.post("/register", jsonParser, function (req, res) {
+  const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+  let encrypted = cipher.update(req.body.password, "utf8", "hex");
+  encrypted += cipher.final("hex");
+
+  console.log("Encrypted Password: ", encrypted);
+
+  const data = new User({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    city: req.body.city,
+    email: req.body.email,
+    password: encrypted,
+    company: req.body.company,
+  });
+
+  data
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => console.warn(error));
+
+  // res.end("User registered successfully with encrypted password");
 });
 
-app.listen(4000);
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
